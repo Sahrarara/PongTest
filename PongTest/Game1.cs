763c6f437c85ac240bab0f62e195c3180bcb4dd2
Sahrarara <sarah.e.git@gmail.com>
2025-03-16 12:46:11 +0100

    Did the monogame tutorial to move an object and have borders

--- PongTest/Game1.cs
+++ PongTest/Game1.cs
@@ -6,6 +6,9 @@ namespace PongTest;
 
 public class Game1 : Game
 {
+    Texture2D ballTexture;
+    Vector2 ballPosition;
+    float ballSpeed;
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
@@ -19,13 +22,20 @@ public class Game1 : Game
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
+        ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2,
+                                   _graphics.PreferredBackBufferHeight / 2);
 
+        ballSpeed = 1000f;
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+
+        ballTexture = Content.Load<Texture2D>("ball");
+
 
         // TODO: use this.Content to load your game content here
     }
@@ -37,6 +47,49 @@ public class Game1 : Game
 
         // TODO: Add your update logic here
 
+        //Time since update was last called
+        //Every frame is a max speed of ballspeed and not more,
+        //otherwise the speed goes out of hand and framerate depicts how fast the game runs
+        float updateBallSpeed = ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+        var kstate = Keyboard.GetState();
+
+        if (kstate.IsKeyDown(Keys.Up))
+        {
+            ballPosition.Y -= updateBallSpeed;
+        }
+        if (kstate.IsKeyDown(Keys.Down))
+        {
+            ballPosition.Y += updateBallSpeed;
+        }
+        if (kstate.IsKeyDown(Keys.Left))
+        {
+            ballPosition.X -= updateBallSpeed;
+        }
+        if (kstate.IsKeyDown(Keys.Right))
+        {
+            ballPosition.X += updateBallSpeed;
+        }
+
+        if (ballPosition.X > _graphics.PreferredBackBufferWidth - ballTexture.Width / 2)
+        {
+            ballPosition.X = _graphics.PreferredBackBufferWidth - ballTexture.Width / 2;
+        }
+        else if (ballPosition.X < ballTexture.Width / 2)
+        {
+            ballPosition.X = 0 + ballTexture.Width /2;
+        }
+
+
+        if (ballPosition.Y > _graphics.PreferredBackBufferHeight - ballTexture.Height / 2)
+        {
+            ballPosition.Y = _graphics.PreferredBackBufferHeight - ballTexture.Height / 2;
+        }
+        else if (ballPosition.Y < ballTexture.Height / 2)
+        {
+            ballPosition.Y =  0 + ballTexture.Height / 2;
+        }
+
         base.Update(gameTime);
     }
 
@@ -45,6 +98,19 @@ public class Game1 : Game
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
         // TODO: Add your drawing code here
+        _spriteBatch.Begin();
+        _spriteBatch.Draw(
+            ballTexture,
+            ballPosition,
+            null,
+            Color.White,
+            0f,
+            new Vector2(ballTexture.Width/2,ballTexture.Height/2),
+            Vector2.One,
+            SpriteEffects.None,
+            0f
+            );
+        _spriteBatch.End();
 
         base.Draw(gameTime);
     }
